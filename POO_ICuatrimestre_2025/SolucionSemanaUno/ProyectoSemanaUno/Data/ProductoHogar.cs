b6aeebf9e1b9a4254dc162c175a271138764d91d
andreypadias <andrey.padiascalvo@gmail.com>
2025-01-22 20:30:19 -0600

    Codigo de semana 1 y 2
    Codigo de semana 1 y 2

---
+++ POO_ICuatrimestre_2025/SolucionSemanaUno/ProyectoSemanaUno/Data/ProductoHogar.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoSemanaUno.Data
+{
+    public class ProductoHogar : Producto
+    {
+        public ProductoHogar(string nombre, decimal precio, int cantidadDisponible)
+            : base(nombre, "Hogar", precio, cantidadDisponible) { }
+
+        public override void MostrarInformacion()
+        {
+            /*Console.WriteLine("Hogar - "+Nombre+Precio+CantidadDisponible);
+            Console.WriteLine("Hogar - Cantidad{0} Nombre:{1} Precio {2}",CantidadDisponible, Nombre, Precio);
+            */
+            Console.WriteLine($"Hogar - {Nombre}: ${Precio} ({CantidadDisponible} disponibles)");
+        }
+    }
+
+  
+
+    
+}
